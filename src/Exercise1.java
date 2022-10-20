07989d6063b9034d15292e8aad46bf4c31b33b42
StefanLuciann <alecu.stefanlucian@yahoo.com>
2022-10-20 19:29:05 +0300

    ok

---
+++ src/Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    //Write an application that will read diameter of a circle (variable of type float) and
+    //calculate perimeter of given circle.
+    //Firstly, assume π = 3.14. Later, use value of π from built-in Math class.
+    //P= πD
+    //D= 2r
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float diametru = scanner.nextFloat();
+         double Perimetru = Math.PI*diametru;
+        System.out.println("Perimetrul este = " + Perimetru);
+    }
+}
